42f55512fb208f8b9a5393fe5e5d77d19d3a4dc8
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-02-12 15:45:56 -0300

    some bug fixes

--- sslt/data/readers/reader.py
+++ sslt/data/readers/reader.py
@@ -1,15 +1,16 @@
 from typing import Any
+
 import numpy as np
 
 
 class _Reader:
     """
-    Base class for readers. Readers define an ordered collection of data and 
+    Base class for readers. Readers define an ordered collection of data and
     provide methods to access it. This class primarily handles:
-    
+
     1. Definition of data structure and storage.
     2. Reading data from the source.
-    
+
     The access is handled by the __getitem__ and __len__ methods, which should be
     implemented by a subclass. Readers usually returns a single item at a time,
     that can be a single image, a single label, etc.
