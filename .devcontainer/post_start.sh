f677a928e3fe811c3171d201df4af0ab979a15cd
Otavio Napoli <otavio.napoli@gmail.com>
2024-02-12 02:16:21 +0000

    Added dockerfile and devcontainer VScode files to allow remote development
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

---
+++ .devcontainer/post_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing SSLT"
+pip install -e .
+
+# Add tmux options
+echo -e "set -g default-terminal \"screen-256color\"\nset -g mouse on\nbind e setw synchronize-panes on\nbind E setw synchronize-panes off" >> ~/.tmux.conf
+
+# remove full path from prompt
+sed -i '/^\s*PS1.*\\w/s/\\w/\\W/g' ~/.bashrc
