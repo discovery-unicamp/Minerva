bd3666dbd2d56b1f309cd0962c734d070b7479fb
Otavio Napoli <otavio.napoli@gmail.com>
2024-02-13 16:12:47 +0000

    Fix typos in documentation and add more docs
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

--- sslt/data/datasets/supervised_dataset.py
+++ sslt/data/datasets/supervised_dataset.py
@@ -8,44 +8,71 @@ from sslt.transforms.transform import _Transform
 
 
 class SupervisedReconstructionDataset(SimpleDataset):
-    """
-    A dataset class for supervised semantic segmentation tasks.
+    """A simple dataset class for supervised reconstruction tasks.
+
+    In summary, each element of the dataset is a pair of data, where the first
+    element is the input data and the second element is the target data.
+    Usually, both input and target data have the same shape.
+
+    This dataset is useful for supervised tasks such as image reconstruction,
+    segmantic segmentation, and object detection, where the input data is the
+    original data and the target is a mask or a segmentation map.
+
+    Examples
+    --------
+
+    1. Semantic Segmentation Dataset:
+
+        ```python
+        from sslt.data.readers import ImageReader
+        from sslt.transforms import ImageTransform
+        from sslt.data.datasets import SupervisedReconstructionDataset
+
+        # Create the readers
+        image_reader = ImageReader("path/to/images")
+        mask_reader = ImageReader("path/to/masks")
+
+        # Create the transforms
+        image_transform = ImageTransform()
+
+        # Create the dataset
+        dataset = SupervisedReconstructionDataset(
+            readers=[image_reader, mask_reader],
+            transforms=image_transform
+        )
+        
+        # Load the first sample
+        dataset[0]  # Returns a tuple: (image, mask)
+        ```
     """
 
-    def __init__(self, readers: List[_Reader], transforms: _Transform | None = None):
-        """
-        Initializes a SupervisedSemanticSegmentationDataset object.
+    def __init__(
+        self, readers: List[_Reader], transforms: _Transform | None = None
+    ):
+        """A simple dataset class for supervised reconstruction tasks.
 
         Parameters
         ----------
-            readers: List[_Reader]
-                List of data readers. It must contain exactly 2 readers.
-                The first reader for the input data and the second reader for the target data.
-            transforms: TransformPipeline | None
-                Optional data transformation pipeline.
+        readers: List[_Reader]
+            List of data readers. It must contain exactly 2 readers.
+            The first reader for the input data and the second reader for the
+            target data.
+        transforms: _Transform | None
+            Optional data transformation pipeline.
 
-        Raises:
+        Raises
+        -------
             AssertionError: If the number of readers is not exactly 2.
         """
         super().__init__(readers, transforms)
 
         assert (
             len(self.readers) == 2
-        ), "SupervisedSemanticSegmentationDataset requires exactly 2 readers"
-
-    def __len__(self) -> int:
-        """
-        Returns the length of the dataset.
-
-        Returns:
-            int: Length of the dataset.
-
-        """
-        return len(self.readers[0])
+        ), "SupervisedReconstructionDataset requires exactly 2 readers"
 
     def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Load data from sources and apply specified transforms.
+        """Load data from sources and apply specified transforms. The same
+        transform is applied to both input and target data.
 
         Parameters
         ----------
