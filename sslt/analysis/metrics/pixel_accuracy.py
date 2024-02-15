5e91b681c7a7c01f9b4ee4d24d2024e7706d25f9
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-02-15 14:22:45 -0300

    Pixel Accuracy

---
+++ sslt/analysis/metrics/pixel_accuracy.py
@@ -0,0 +1,43 @@
+import torch
+from torchmetrics import Metric
+
+
+class PixelAccuracy(Metric):
+    def __init__(self, dist_sync_on_step=False):
+        """
+        Initializes a PixelAccuracy metric object.
+
+        Parameters
+        ----------
+            dist_sync_on_step: bool | None
+                Whether to synchronize metric state across processes at each step.
+                Defaults to False.
+        """
+        super().__init__(dist_sync_on_step=dist_sync_on_step)
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        """
+        Updates the metric state with the predictions and targets.
+
+        Parameters
+        ----------
+            preds: torch.Tensor
+                The predicted tensor.
+            target:
+                torch.Tensor The target tensor.
+        """
+        correct = torch.sum(preds == target)
+        total = target.numel()
+        self.correct += correct
+        self.total += total
+
+    def compute(self) -> float:
+        """
+        Computes the pixel accuracy.
+
+        Returns:
+            float: The pixel accuracy.
+        """
+        return self.correct.float() / self.total
