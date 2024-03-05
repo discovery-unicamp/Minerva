c2b7885a733d395a6a5c0e29fc6c41a4878b87c9
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-03-05 14:32:57 -0300

    tests setr

--- tests/models/nets/test_setr.py
+++ tests/models/nets/test_setr.py
@@ -3,7 +3,7 @@ import torch
 from sslt.models.nets.setr import _SetR_PUP
 
 if __name__ == "__main__":
-    model = _SetR_PUP(2, 3, 4, 5, 6, 7, 8, 9, 10)
+    model = _SetR_PUP(512, 16, 24, 16, 1, 1, 3)
     print(model)
     result = model.forward(torch.zeros(1, 2, 3, 4))
     print(result.shape)
