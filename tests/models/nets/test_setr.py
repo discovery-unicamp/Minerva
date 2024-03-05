85e2a879ee462d3112991f217132d0b6a40dde41
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-03-05 14:37:24 -0300

    testes setr

--- tests/models/nets/test_setr.py
+++ tests/models/nets/test_setr.py
@@ -3,7 +3,15 @@ import torch
 from sslt.models.nets.setr import _SetR_PUP
 
 if __name__ == "__main__":
-    model = _SetR_PUP(512, 16, 24, 16, 1, 1, 3)
+    model = _SetR_PUP(
+        image_size=512,
+        patch_size=16,
+        num_layers=24,
+        num_heads=16,
+        hidden_dim=2,
+        mlp_dim=1,
+        num_classes=3,
+    )
     print(model)
     result = model.forward(torch.zeros(1, 2, 3, 4))
     print(result.shape)
