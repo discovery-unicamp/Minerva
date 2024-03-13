2e5eeaefbd9ac8b1a6ad5b0c0724c05f3a083e8d
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-03-12 22:53:03 -0300

    Update SETR_PUP model and add docstrings to Vision Transformer Backbone class

--- test_setr.py
+++ test_setr.py
@@ -1,10 +1,10 @@
 import torch
 
-from sslt.models.nets.setr import _SetR_PUP
+from sslt.models.nets.setr import SETR_PUP
 
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = _SetR_PUP(
+    model = SETR_PUP(
         image_size=(16, 32),
         patch_size=16,
         num_layers=24,
@@ -12,6 +12,13 @@ if __name__ == "__main__":
         hidden_dim=1024,
         mlp_dim=1,
         num_classes=3,
+        num_convs=4,
+        decoder_channels=256,
+        up_scale=4,
+        encoder_dropout=0.1,
+        kernel_size=3,
+        decoder_dropout=0.1,
+        conv_act=torch.nn.ReLU(inplace=True),
     )
     model.to(device)
     result = model.forward(torch.zeros(1, 3, 16, 32).to(device))
