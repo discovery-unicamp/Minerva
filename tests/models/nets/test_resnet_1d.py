754dabfc6de210976a825513d44c430f89f6b3f9
Otavio Napoli <otavio.napoli@gmail.com>
2024-05-29 10:41:25 +0000

    Added tests to all models added
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

---
+++ tests/models/nets/test_resnet_1d.py
@@ -0,0 +1,42 @@
+import torch
+from minerva.models.nets.resnet_1d import ResNet1D_8, ResNetSE1D_8, ResNetSE1D_5
+
+def test_resnet_1d_8_forward():
+    input_shape = (6, 60)
+    model = ResNet1D_8(
+        input_shape=input_shape,
+        num_classes=6,
+        learning_rate=1e-3,
+    )
+    assert model is not None
+
+    x = torch.rand(1, *input_shape)
+    y = model(x)
+    assert y is not None
+    
+    
+def test_resnet_se_1d_8_forward():
+    input_shape = (6, 60)
+    model = ResNetSE1D_8(
+        input_shape=input_shape,
+        num_classes=6,
+        learning_rate=1e-3,
+    )
+    assert model is not None
+
+    x = torch.rand(1, *input_shape)
+    y = model(x)
+    assert y is not None
+    
+def test_resnet_se_1d_5_forward():
+    input_shape = (6, 60)
+    model = ResNetSE1D_5(
+        input_shape=input_shape,
+        num_classes=6,
+        learning_rate=1e-3,
+    )
+    assert model is not None
+
+    x = torch.rand(1, *input_shape)
+    y = model(x)
+    assert y is not None
