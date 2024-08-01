15d918f365566bda2331d95a968c7e949b81fb23
gustavo luz <58995773+gustavo-luz@users.noreply.github.com>
2024-08-01 11:39:02 -0300

    makes expected type of adapter more flexible

--- minerva/models/nets/base.py
+++ minerva/models/nets/base.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional, Union
+from typing import Dict, Optional, Union, Any
 
 import lightning as L
 import torch
@@ -30,7 +30,7 @@ class SimpleSupervisedModel(L.LightningModule):
         backbone: Union[torch.nn.Module, LoadableModule],
         fc: Union[torch.nn.Module, LoadableModule],
         loss_fn: torch.nn.Module,
-        adapter: Optional[callable] = None,
+        adapter: Any,
         learning_rate: float = 1e-3,
         flatten: bool = True,
         train_metrics: Optional[Dict[str, Metric]] = None,
