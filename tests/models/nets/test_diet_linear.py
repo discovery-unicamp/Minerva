f723a1f5ee0361a9cd64f267c8a6d0831475f116
Hubert <darlinnep@gmail.com>
2024-08-07 15:49:23 -0300

    Updated test

--- tests/models/nets/test_diet_linear.py
+++ tests/models/nets/test_diet_linear.py
@@ -2,9 +2,9 @@ from minerva.models.nets.diet_linear import DIETLinear
 import torch
 
 def test_diet_linear():
-    model = DIETLinear(in_features==256, out_features==100)
+    model = DIETLinear(in_features=256, out_features=100)
     assert model is not None
 
-    x = torch.rand(32, 256, 100)
+    x = torch.rand(32, 256)
     y = model(x)
     assert y is not None
