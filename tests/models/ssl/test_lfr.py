1c8dc87e3eccf9fc4e6d12f273d66a9fd87c0158
Hubert <darlinnep@gmail.com>
2024-08-12 10:06:46 -0300

    Added custom loss, nets, model, and tests

---
+++ tests/models/ssl/test_lfr.py
@@ -0,0 +1,20 @@
+from minerva.models.ssl.lfr import LearnFromRandomnessModel
+from minerva.models.nets.lfr_har_architectures import LFR_HAR_Backbone, LFR_HAR_Projector, LFR_HAR_Predictor
+import torch
+
+
+def test_lfr():
+    model = LearnFromRandomnessModel(
+        backbone=LFR_HAR_Backbone(encoding_size=256, input_channel=6),
+        projectors=torch.nn.ModuleList([LFR_HAR_Projector(encoding_size=256, input_channel=6) for _ in range(8)]),
+        predictors=torch.nn.ModuleList([LFR_HAR_Predictor(encoding_size=256, middle_dim=128, num_layers=3) for _ in range(8)]),
+        loss_fn=None,
+        learning_rate=1e-3,
+        flatten=True,
+        predictor_training_epochs=7
+    )
+    assert model is not None
+
+    x = torch.rand(32, 6, 60)
+    y = model(x)
+    assert y is not None
