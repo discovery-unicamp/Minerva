a39f3c5e34f236752a1aaffb1d44e6664836e31d
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-09-01 12:33:25 -0300

    feat: Add TrainerReportKeepOnlyLastCallback for saving and reporting the last checkpoint during training and deleting the old ones

--- minerva/callbacks/HyperSearchCallbacks.py
+++ minerva/callbacks/HyperSearchCallbacks.py
@@ -55,3 +55,50 @@ class TrainerReportOnIntervalCallback(L.Callback):
 
         if self.local_rank == 0:
             shutil.rmtree(tmpdir)
+
+
+class TrainerReportKeepOnlyLastCallback(L.Callback):
+
+    CHECKPOINT_NAME = "checkpoint.ckpt"
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.trial_name = train.get_context().get_trial_name()
+        self.local_rank = train.get_context().get_local_rank()
+        self.tmpdir_prefix = Path(tempfile.gettempdir(), self.trial_name).as_posix()
+        if os.path.isdir(self.tmpdir_prefix) and self.local_rank == 0:
+            shutil.rmtree(self.tmpdir_prefix)
+
+        record_extra_usage_tag(TagKey.TRAIN_LIGHTNING_RAYTRAINREPORTCALLBACK, "1")
+
+    def on_train_epoch_end(
+        self, trainer: L.Trainer, pl_module: L.LightningModule
+    ) -> None:
+        # Fetch metrics
+        metrics = trainer.callback_metrics
+        metrics = {k: v.item() for k, v in metrics.items()}
+
+        # (Optional) Add customized metrics
+        metrics["epoch"] = trainer.current_epoch
+        metrics["step"] = trainer.global_step
+
+        tmpdir = Path(self.tmpdir_prefix, "last").as_posix()
+        os.makedirs(tmpdir, exist_ok=True)
+
+        # Delete previous checkpoint
+        if os.path.isdir(tmpdir):
+            shutil.rmtree(tmpdir)
+
+        # Save checkpoint to local
+        ckpt_path = Path(tmpdir, self.CHECKPOINT_NAME).as_posix()
+        trainer.save_checkpoint(ckpt_path, weights_only=False)
+
+        # Report to train session
+        checkpoint = Checkpoint.from_directory(tmpdir)
+        train.report(metrics=metrics, checkpoint=checkpoint)
+
+        # Add a barrier to ensure all workers finished reporting here
+        trainer.strategy.barrier()
+
+        if self.local_rank == 0:
+            shutil.rmtree(tmpdir)
