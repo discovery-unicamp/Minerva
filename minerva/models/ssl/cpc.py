14381634c6712a36b26400c453532ad20fe450ac
betaniarodrigues <betania_rodrigues1997@hotmail.com>
2024-09-18 00:46:47 +0000

    Cpc model optimization

--- minerva/models/ssl/cpc.py
+++ minerva/models/ssl/cpc.py
@@ -1,12 +1,14 @@
 from torch import nn, optim
 import lightning as L
 import torch
+from torch.optim.lr_scheduler import StepLR
 
 class CPC(L.LightningModule):
     """Implements the Contrastive Predictive Coding (CPC) model, as described in
     https://dl.acm.org/doi/10.1145/3463506. The implementation was adapted from
     https://github.com/harkash/contrastive-predictive-coding-for-har
     """
+
     def __init__(
         self,
         g_enc: L.LightningModule,
@@ -14,14 +16,14 @@ class CPC(L.LightningModule):
         prediction_head: L.LightningModule,
         num_steps_prediction: int = 28,
         batch_size: int = 64,
-        learning_rate: float = 1e-3,
-    ):  
-        """       
+        learning_rate: float = 5e-4,
+    ):
+        """
         Parameters
         ----------
-        g_enc : L.LightningModule
+        g_enc :  L.LightningModule
             Encoder network that processes the input sequence and extracts features.
-        g_ar : L.LightningModule
+        g_ar :  L.LightningModule
             Autoregressive network that models the temporal dependencies in the feature space.
         prediction_head : L.LightningModule
             Network used to predict future representations.
@@ -30,14 +32,13 @@ class CPC(L.LightningModule):
         batch_size : int, optional
             Size of the batch, by default 64.
         learning_rate : float, optional
-            Learning rate for the optimizer, by default 1e-3.
+            Learning rate for the optimizer, by default 5e-4.
         """
         super(CPC, self).__init__()
         self.g_enc = g_enc
         self.g_ar = g_ar
         self.prediction_head = prediction_head
-        self.predictors = nn.ModuleList([self.prediction_head
-                                 for i in range(num_steps_prediction)])
+        self.predictors = nn.ModuleList([self.prediction_head for _ in range(num_steps_prediction)])
         self.learning_rate = learning_rate
         self.softmax = nn.Softmax(dim=1)
         self.lsoftmax = nn.LogSoftmax(dim=1)
@@ -46,115 +47,60 @@ class CPC(L.LightningModule):
 
     def forward(self, x):
         z = self.g_enc(x)
-        r_out, _ = self.g_ar(z, None)
-        r_out = r_out[:, -1, :]
-        y = torch.stack([pred(r_out) for pred in self.predictors], dim=1)
-        return z, y
-    
-    def training_step(self, batch, batch_idx):
-        z, _ = self.forward(batch[0])
-        batch_size = batch[0].size(0) 
-        S = z.size(1)        
-        # Random time step to start future prediction
-        start = torch.randint(high=int(S - self.num_steps_prediction),
-                              size=(1,), low=2).long() 
-                      
-        # Context: Input data up to the start time (PAST)
-        context = batch[0][:, :, :start+1]
+        S = x.size(2)
+        start = (
+            torch.randint(int(S - self.num_steps_prediction), size=(1,)).long().item()
+        )
+        rnn_input = z[:, : start + 1, :]
+        r_out, (_) = self.g_ar(rnn_input, None)
+        return z, r_out, start
 
-        # Ground truth: Input data from the start time + number of prediction steps (FUTURE)
-        y_truth = z[:, start+1:start+1+self.num_steps_prediction, :].permute(1, 0, 2)
-
-        _, y_pred = self.forward(context)
-
-        y_pred = y_pred.permute(1, 0, 2)
-
+    def compute_nce_loss(self, z, r_out, start):
+        batch_size = z.size(0)
+        context = r_out[:, start, :].squeeze(1)
+        y_pred = torch.stack(
+            [
+                self.predictors[pred](context)
+                for pred in range(self.num_steps_prediction)
+            ]
+        )
+        y_truth = z[:, start + 1 : start + 1 + self.num_steps_prediction, :].permute(
+            1, 0, 2
+        )
         nce = 0
         correct = 0
-        correct_steps = []
 
-        # Looping over the number of timesteps chosen
         for k in range(self.num_steps_prediction):
-            log_density_ratio = torch.mm(y_truth[k],y_pred[k].transpose(0, 1))
-
-            # correct if highest probability is in the diagonal
-            positive_batch_pred = torch.argmax(self.softmax(log_density_ratio),
-                                               dim=0)
+            log_density_ratio = torch.mm(y_truth[k], y_pred[k].transpose(0, 1))
+            positive_batch_pred = torch.argmax(self.softmax(log_density_ratio), dim=0)
             positive_batch_actual = torch.arange(0, batch_size).to(self.device)
+            correct += torch.sum(
+                torch.eq(positive_batch_pred, positive_batch_actual)
+            ).item()
+            nce += torch.sum(torch.diag(self.lsoftmax(log_density_ratio)))
 
-            correct = (correct +
-                       torch.sum(torch.eq(positive_batch_pred,
-                                          positive_batch_actual)).item())
-            correct_steps.append(torch.sum(torch.eq(positive_batch_pred,
-                                                    positive_batch_actual)).item())
-
-            # calculate NCE loss
-            nce = nce + torch.sum(torch.diag(self.lsoftmax(log_density_ratio)))
-
-        # average over timestep and batch
         nce = nce / (-1.0 * batch_size * self.num_steps_prediction)
+        return nce
+
+    def training_step(self, batch, batch_idx):
+        z, r_out, start = self.forward(batch[0])
+        nce = self.compute_nce_loss(z, r_out, start)
         self.log("train_loss", nce)
         return nce
 
     def validation_step(self, batch, batch_idx):
-        z, _ = self.forward(batch[0]) 
-        batch_size = batch[0].size(0)
-        S = z.size(1)
-        start = torch.randint(high=int(S - self.num_steps_prediction),low=2,
-                              size=(1,)).long()     
-        context = batch[0][:, :, :start+1]
-        y_truth = z[:, start+1:start+1+self.num_steps_prediction, :].permute(1, 0, 2) 
-        _, y_pred = self.forward(context)
-        y_pred = y_pred.permute(1, 0, 2)
-        nce = 0
-        correct = 0
-        correct_steps = []
-        for k in range(self.num_steps_prediction):
-            log_density_ratio = torch.mm(y_truth[k],y_pred[k].transpose(0, 1))
-            positive_batch_pred = torch.argmax(self.softmax(log_density_ratio),
-                                               dim=0)
-            positive_batch_actual = torch.arange(0, batch_size).to(self.device)
-            correct = (correct +
-                       torch.sum(torch.eq(positive_batch_pred,
-                                          positive_batch_actual)).item())
-            correct_steps.append(torch.sum(torch.eq(positive_batch_pred,
-                                                    positive_batch_actual)).item())
-            nce = nce + torch.sum(torch.diag(self.lsoftmax(log_density_ratio)))
-        nce = nce / (-1.0 * batch_size * self.num_steps_prediction)
+        z, r_out, start = self.forward(batch[0])
+        nce = self.compute_nce_loss(z, r_out, start)
         self.log("val_loss", nce)
         return nce
 
     def test_step(self, batch, batch_idx):
-        z, _ = self.forward(batch[0]) 
-        batch_size = batch[0].size(0)
-        S = z.size(1)
-        start = torch.randint(high=int(S - self.num_steps_prediction),low=2,
-                              size=(1,)).long()
-        context = batch[0][:, :, :start+1]
-        y_truth = z[:, start+1:start+1+self.num_steps_prediction, :].permute(1, 0, 2) 
-        _, y_pred = self.forward(context)
-        y_pred = y_pred.permute(1, 0, 2)
-        nce = 0
-        correct = 0
-        correct_steps = []
-        for k in range(self.num_steps_prediction):
-            log_density_ratio = torch.mm(y_truth[k],y_pred[k].transpose(0, 1))
-            positive_batch_pred = torch.argmax(self.softmax(log_density_ratio),
-                                               dim=0)
-            positive_batch_actual = torch.arange(0, batch_size).to(self.device)
-            correct = (correct +
-                       torch.sum(torch.eq(positive_batch_pred,
-                                          positive_batch_actual)).item())
-            correct_steps.append(torch.sum(torch.eq(positive_batch_pred,
-                                                    positive_batch_actual)).item())
-            nce = nce + torch.sum(torch.diag(self.lsoftmax(log_density_ratio)))
-        nce = nce / (-1.0 * batch_size * self.num_steps_prediction)
+        z, r_out, start = self.forward(batch[0])
+        nce = self.compute_nce_loss(z, r_out, start)
         self.log("test_loss", nce)
         return nce
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(
-            self.parameters(),
-            lr=self.learning_rate,
-        )
-        return optimizer
+        optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
+        scheduler = StepLR(optimizer, step_size=25, gamma=0.8)
+        return [optimizer], [scheduler]
