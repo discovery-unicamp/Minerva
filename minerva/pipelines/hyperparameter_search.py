030ee5d49fef89f2de69e573a40003da88b71844
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-09-23 11:30:44 -0300

    Refactor hyperparameter search pipeline

--- minerva/pipelines/hyperparameter_search.py
+++ minerva/pipelines/hyperparameter_search.py
@@ -3,18 +3,17 @@ from copy import deepcopy
 from typing import Any, Dict, Literal, Optional
 
 import lightning.pytorch as L
+from lightning.pytorch.strategies import Strategy
 from ray import tune
 from ray.train import CheckpointConfig, RunConfig, ScalingConfig
 from ray.train.lightning import RayDDPStrategy, RayLightningEnvironment, prepare_trainer
 from ray.train.torch import TorchTrainer
-from ray.tune.schedulers import ASHAScheduler
+from ray.tune.schedulers import ASHAScheduler, TrialScheduler
 from ray.tune.search.sample import Categorical, Float, Integer
+from torch.optim import SGD, Adam, AdamW, RAdam
 from torchmetrics import Metric
 
-from minerva.callbacks.HyperSearchCallbacks import (
-    TrainerReportOnIntervalCallback,
-    TrainerReportKeepOnlyLastCallback,
-)
+from minerva.callbacks.HyperSearchCallbacks import TrainerReportKeepOnlyLastCallback
 from minerva.pipelines.base import Pipeline
 from minerva.utils.typing import PathLike
 
@@ -36,64 +35,57 @@ class HyperParameterSearch(Pipeline):
         self,
         data: L.LightningDataModule,
         ckpt_path: Optional[PathLike],
-        configs: Dict[str, Any],
+        devices: Optional[str] = "auto",
+        accelerator: Optional[str] = "auto",
+        strategy: Optional[Strategy] = None,
+        callbacks: Optional[Any] = None,
+        plugins: Optional[Any] = None,
+        num_nodes: int = 1,
+        debug_mode: Optional[bool] = False,
+        scaling_config: Optional[ScalingConfig] = None,
+        run_config: Optional[RunConfig] = None,
+        tuner_metric: Optional[str] = "val_loss",
+        tuner_mode: Optional[str] = "min",
+        num_samples: Optional[int] = 10,
+        scheduler: Optional[TrialScheduler] = None,
     ) -> Any:
 
         def _tuner_train_func(config):
             dm = deepcopy(data)
             model = self.model(config_dict=config)
             trainer = L.Trainer(
-                devices=configs.get("devices", "auto"),
-                accelerator=configs.get("accelerator", "auto"),
-                strategy=configs.get(
-                    "strategy", RayDDPStrategy(find_unused_parameters=True)
-                ),
-                callbacks=configs.get(
-                    "callbacks", [TrainerReportKeepOnlyLastCallback()]
-                ),
-                plugins=configs.get("plugins", [RayLightningEnvironment()]),
+                devices=devices or "auto",
+                accelerator=accelerator or "auto",
+                strategy=strategy or RayDDPStrategy(find_unused_parameters=True),
+                callbacks=callbacks or [TrainerReportKeepOnlyLastCallback()],
+                plugins=plugins or [RayLightningEnvironment()],
                 enable_progress_bar=False,
-                num_nodes=configs.get("num_nodes", 1),
-                enable_checkpointing=(
-                    False if configs.get("debug_mode") is True else None
-                ),
+                num_nodes=num_nodes,
+                enable_checkpointing=False if debug_mode else None,
             )
             trainer = prepare_trainer(trainer)
             trainer.fit(model, dm, ckpt_path=ckpt_path)
 
-        scheduler = configs.get(
-            "scheduler",
-            ASHAScheduler(
-                time_attr=configs.get("time_attr", "training_iteration"),
-                max_t=configs.get("max_t", 2),
-                grace_period=configs.get("grace_period", 1),
-                brackets=configs.get("brackets", 1),
-            ),
+        scheduler = scheduler or ASHAScheduler(
+            time_attr="training_iteration",
+            metric=tuner_metric or "val_loss",
+            mode=tuner_mode or "min",
+            max_t=2,
+            grace_period=1,
+            brackets=1,
         )
 
-        scaling_config = configs.get(
-            "scaling_config",
-            ScalingConfig(num_workers=1, use_gpu=True, resources_per_worker={"GPU": 1}),
+        scaling_config = scaling_config or ScalingConfig(
+            num_workers=1, use_gpu=True, resources_per_worker={"GPU": 1}
         )
 
-        run_config = configs.get(
-            "run_config",
-            RunConfig(
-                checkpoint_config=(
-                    CheckpointConfig(
-                        num_to_keep=configs.get("num_to_keep", 1),
-                        checkpoint_score_attribute=configs.get(
-                            "tuner_metric", "val_loss"
-                        ),
-                        checkpoint_score_order=configs.get("tuner_mode", "min"),
-                        checkpoint_frequency=(
-                            configs.get("checkpoint_frequency", 10)
-                            if configs.get("debug_mode", False) is False
-                            else 0
-                        ),
-                    )
-                ),
-            ),
+        run_config = run_config or RunConfig(
+            checkpoint_config=CheckpointConfig(
+                num_to_keep=1,
+                checkpoint_score_attribute="val_loss",
+                checkpoint_score_order="min",
+                checkpoint_frequency=10,
+            )
         )
 
         ray_trainer = TorchTrainer(
@@ -105,9 +97,9 @@ class HyperParameterSearch(Pipeline):
             ray_trainer,
             param_space={"train_loop_config": self.search_space},
             tune_config=tune.TuneConfig(
-                metric=configs.get("tuner_metric", "val_loss"),
-                mode=configs.get("tuner_mode", "min"),
-                num_samples=configs.get("num_samples", 10),
+                metric=tuner_metric or "val_loss",
+                mode=tuner_mode or "min",
+                num_samples=num_samples or 10,
                 scheduler=scheduler,
             ),
         )
@@ -122,13 +114,24 @@ class HyperParameterSearch(Pipeline):
         data: L.LightningDataModule,
         task: Optional[Literal["search", "test", "predict"]],
         ckpt_path: Optional[PathLike] = None,
-        configs: Dict[str, Any] = {},
+        **kwargs,
     ) -> Any:
         if task == "search":
-            return self._search(data, ckpt_path, configs)
+            return self._search(data, ckpt_path, **kwargs)
         elif task == "test":
             return self._test(data, ckpt_path)
         elif task is None:
-            search = self._search(data, ckpt_path, configs)
+            search = self._search(data, ckpt_path, **kwargs)
             test = self._test(data, ckpt_path)
             return search, test
+
+
+def main():
+    from jsonargparse import CLI
+
+    print("Hyper Searching 🔍")
+    CLI(HyperParameterSearch, as_positional=False)
+
+
+if __name__ == "__main__":
+    main()
