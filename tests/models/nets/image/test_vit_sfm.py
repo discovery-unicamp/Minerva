e63bbe52a03f973e33e6093e1ba1b60cb4e09f5c
Otavio Napoli <otavio.napoli@gmail.com>
2024-12-09 12:12:11 +0000

    Refactor tests that uses lightning Trainer to run on single-CPU

--- tests/models/nets/image/test_vit_sfm.py
+++ tests/models/nets/image/test_vit_sfm.py
@@ -8,7 +8,7 @@ from minerva.models.nets.image.vit import (
     mae_vit_large_patch16,
     mae_vit_large_patch16D4d256,
     mae_vit_small_patch16,
-    SFM_BasePatch16_Downstream
+    SFM_BasePatch16_Downstream,
 )
 
 test_models = [
@@ -22,6 +22,7 @@ test_models = [
 
 from minerva.data.data_module_tools import RandomDataModule
 import lightning as L
+
 
 @pytest.mark.parametrize("model_cls,img_size", test_models)
 def test_sfm_pretrain_forward(model_cls, img_size):
@@ -46,11 +47,9 @@ def test_sfm_pretrain_forward(model_cls, img_size):
 
 def test_sfm_vit_patch16_downstream():
     model = SFM_BasePatch16_Downstream(
-        img_size=(512, 512),
-        num_classes=6,
-        in_chans=1
+        img_size=(512, 512), num_classes=6, in_chans=1
     )
-    
+
     data_module = RandomDataModule(
         data_shape=(1, 512, 512),
         label_shape=(1, 512, 512),
@@ -59,5 +58,7 @@ def test_sfm_vit_patch16_downstream():
         batch_size=2,
     )
 
-    trainer = L.Trainer(accelerator="cpu", max_epochs=1, devices=1, fast_dev_run=True)
+    trainer = L.Trainer(
+        accelerator="cpu", max_epochs=1, devices=1, fast_dev_run=True
+    )
     trainer.fit(model, data_module)
