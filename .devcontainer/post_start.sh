d8102e0ee3dddedc9a0ffb66098269db2e633b63
Otavio Napoli <otavio.napoli@gmail.com>
2025-06-09 08:32:55 -0300

    Updated .devcontainer with a small footprint container. Also added conda environment
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Add /home/vscode/.local/bin directory to PATH
-export PATH="/home/vscode/.local/bin':$PATH" >> ~/.bashrc
-
 # Install minerva in editable mode
 echo "Installing minerva develop (in editable mode)..."
-pip install -e .[dev]
+pip install -e ".[dev]"
 echo "Minerva installed in editable mode"
